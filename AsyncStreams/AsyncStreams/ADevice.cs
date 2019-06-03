0591cb8807102e8bc8369ecda3e71d253e139d06
Christian Nagel <christian@christiannagel.com>
2019-06-03 13:08:42 +0200

    async streams

---
+++ AsyncStreams/AsyncStreams/ADevice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    public class ADevice
+    {
+        public async IAsyncEnumerable<SensorData> GetDataStreamAsync()
+        {
+            var r = new Random();
+            while (true)
+            {
+                yield return new SensorData(r.Next(20), r.Next(40));
+                await Task.Delay(r.Next(200));
+            }
+        }
+
+        public async IAsyncEnumerable<SensorData> GetDataStreamWithCancellationAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var r = new Random();
+            while (true)
+            {
+                yield return new SensorData(r.Next(20), r.Next(40));
+                await Task.Delay(r.Next(200));
+                //if (cancellationToken.IsCancellationRequested)
+                //{
+                //    // cleanup
+                //}
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+    }
+}
---
+++ AsyncStreams/AsyncStreams/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // await SimpleAsyncStreamAsync();
+            await SimpleStreamWithCancellationAsync();
+        }
+
+        private async static Task SimpleStreamAsync()
+        {
+            var device = new ADevice();
+            await foreach (var data in device.GetDataStreamAsync())
+            {
+                Console.WriteLine($"{data.Value1} {data.Value2}");
+            }
+        }
+
+        private async static Task SimpleStreamWithCancellationAsync()
+        {
+            try
+            {
+                var cts2 = new CancellationTokenSource();
+                cts2.CancelAfter(5000);
+                cts2.Token.Register(() =>
+                {
+                    Console.WriteLine("cancel cts2 now...");
+                });
+                var cts = new CancellationTokenSource();
+                cts.Token.Register(() =>
+                {
+                    Console.WriteLine("cancel now...");
+                });
+                cts.CancelAfter(3000);
+                var device = new ADevice();
+                await foreach (var data in device.GetDataStreamWithCancellationAsync(cts2.Token).WithCancellation(cts.Token))
+                {
+                    Console.WriteLine($"{data.Value1} {data.Value2}");
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
