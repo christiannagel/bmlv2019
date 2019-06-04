0aa914793eee091bab5be08c70e538c59cb4ece5
Christian Nagel <christian@christiannagel.com>
2019-06-04 13:38:30 +0200

    grpc

---
+++ grpcsample/grpcclient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace grpcclient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("client - wait for server");
+            Console.ReadLine();
+            var channel = new Channel("localhost:50051", SslCredentials.Insecure);
+            var client = new Greet.Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new Greet.HelloRequest() { Name = "Katharina" });
+            Console.WriteLine(reply.Message);
+            await channel.ShutdownAsync();
+        }
+    }
+}
