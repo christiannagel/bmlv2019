b3eefb3d81c4845d520b5c9f0062488dcb796958
Christian Nagel <christian@christiannagel.com>
2019-06-05 13:21:21 +0200

    .net native

---
+++ nativedemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nativedemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
